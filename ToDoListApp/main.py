c9449936260ec175357476a3f768414cc2644e2e
Mark Anderson <markhanderson@gmail.com>
2023-05-06 09:26:20 -0500

    Changed to elif

--- ToDoListApp/main.py
+++ ToDoListApp/main.py
@@ -3,67 +3,65 @@ while True:
     user_action = input("Type Add, Show, Edit, Complete or Exit: ")
     user_action = user_action.strip()
 
-    match user_action:
-        case 'Add':
-            task = input("Enter a Task: ") + "\n"
+    if 'Add' in user_action:
+        task = user_action[4:]
 
-            with open('tasks.txt', 'r') as file:
-                tasks = file.readlines()
+        with open('tasks.txt', 'r') as file:
+            tasks = file.readlines()
 
-            tasks.append(task)
+        tasks.append(task)
 
-            with open('tasks.txt', 'w') as file:
-                file.writelines(tasks)
-
-            file = open('tasks.txt', 'w')
+        with open('tasks.txt', 'w') as file:
             file.writelines(tasks)
-            file.close()
 
-        case 'Show':
+        # file = open('tasks.txt', 'w')
+        # file.writelines(tasks)
+        # file.close()
 
-            with open('tasks.txt', 'r') as file:
-                tasks = file.readlines()
-
-
-            for index, item in enumerate(tasks):
-                item = item.strip('\n')
-                row = f"{index + 1}-{item}"
-                print(row)
-
-        case 'Edit':
-            number = int(input("Number of Task to Edit"))
-            number = number - 1
-
-            with open('tasks.txt', 'r') as file:
-                tasks = file.readlines()
-            print('Here are the existing tasks', tasks)
+    elif 'Show' in user_action:
+        with open('tasks.txt', 'r') as file:
+            tasks = file.readlines()
 
 
+        for index, item in enumerate(tasks):
+            item = item.strip('\n')
+            row = f"{index + 1}-{item}"
+            print(row)
 
-            new_task = input("Enter New Task: ")
-            tasks[number] = new_task + '\n'
+    elif 'Edit' in user_action:
+        number = int(user_action[5:])
+        number = number - 1
 
-            with open('tasks.txt', 'w') as file:
-                file.writelines(tasks)
+        with open('tasks.txt', 'r') as file:
+            tasks = file.readlines()
 
-        case 'Complete':
-            number = int(input("Number of Task to Complete"))
+        new_task = input("Enter New Task: ")
+        tasks[number] = new_task + '\n'
 
-            with open('tasks.txt', 'r') as file:
-                tasks = file.readlines()
-            index = number - 1
-            tasks_to_remove = tasks[index].strip('\n')
-            tasks.pop(number - 1)
+        with open('tasks.txt', 'w') as file:
+            file.writelines(tasks)
 
-            with open('tasks.txt', 'w') as file:
-                file.writelines(tasks)
+    elif 'Complete' in user_action:
+        number = int(user_action[9:])
 
-            message = f'task {tasks_to_remove} was removed from the list.'
-            print(message)
+        with open('tasks.txt', 'r') as file:
+            tasks = file.readlines()
+        index = number - 1
+        tasks_to_remove = tasks[index].strip('\n')
+        tasks.pop(number - 1)
 
-        case 'Exit':
-            print("You Entered a Unknown Command, Please Try Again.")
-            break
+        with open('tasks.txt', 'w') as file:
+            file.writelines(tasks)
+
+        message = f'task {tasks_to_remove} was removed from the list.'
+        print(message)
+
+    elif 'Exit' in user_action:
+        break
+
+    else:
+        print('Command is not valid.')
+
 
 print("Bye!")
 
