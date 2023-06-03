baef4711c559763ab2267647b0c948d8d1d73aaf
Mark Anderson <markhanderson@gmail.com>
2023-06-03 17:17:53 -0500

    Fixed Bugs and Added Error Handeling

--- ToDoListApp/main.py
+++ ToDoListApp/main.py
@@ -3,18 +3,18 @@ while True:
     user_action = input("Type Add, Show, Edit, Complete or Exit: ")
     user_action = user_action.strip()
 
-    if 'Add' in user_action or 'New' in user_action:
+    if user_action.startswith("Add"):
         task = user_action[4:]
 
         with open('tasks.txt', 'r') as file:
             tasks = file.readlines()
 
-        tasks.append(task)
+        tasks.append(task + '\n')
 
         with open('tasks.txt', 'w') as file:
             file.writelines(tasks)
 
-    elif 'Show' in user_action:
+    elif user_action.startswith('Show'):
         with open('tasks.txt', 'r') as file:
             tasks = file.readlines()
 
@@ -24,35 +24,43 @@ while True:
             row = f"{index + 1}-{item}"
             print(row)
 
-    elif 'Edit' in user_action:
-        number = int(user_action[5:])
-        number = number - 1
+    elif user_action.startswith('Edit'):
+        try:
+            number = int(user_action[5:])
+            number = number - 1
 
-        with open('tasks.txt', 'r') as file:
-            tasks = file.readlines()
+            with open('tasks.txt', 'r') as file:
+                    tasks = file.readlines()
 
-        new_task = input("Enter New Task: ")
-        tasks[number] = new_task + '\n'
+            new_task = input("Enter New Task: ")
+            tasks[number] = new_task + '\n'
 
-        with open('tasks.txt', 'w') as file:
-            file.writelines(tasks)
+            with open('tasks.txt', 'w') as file:
+                    file.writelines(tasks)
+        except ValueError:
+            print("Your Command is Not Valid, Please try again.")
+            continue
 
-    elif 'Complete' in user_action:
-        number = int(user_action[9:])
+    elif user_action.startswith('Complete'):
+        try:
+            number = int(user_action[9:])
 
-        with open('tasks.txt', 'r') as file:
-            tasks = file.readlines()
-        index = number - 1
-        tasks_to_remove = tasks[index].strip('\n')
-        tasks.pop(number - 1)
+            with open('tasks.txt', 'r') as file:
+                tasks = file.readlines()
+            index = number - 1
+            tasks_to_remove = tasks[index].strip('\n')
+            tasks.pop(number - 1)
 
-        with open('tasks.txt', 'w') as file:
-            file.writelines(tasks)
+            with open('tasks.txt', 'w') as file:
+                file.writelines(tasks)
 
-        message = f'task {tasks_to_remove} was removed from the list.'
-        print(message)
+            message = f'task {tasks_to_remove} was removed from the list.'
+            print(message)
+        except IndexError:
+            print("There is No Task with that Number, please try again. ")
+        continue
 
-    elif 'Exit' in user_action:
+    elif user_action.startswith('Exit'):
         break
 
     else:
