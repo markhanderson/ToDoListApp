64e13f75796dfc33412d2bc103ab01cc7ac26f70
Mark Anderson <markhanderson@gmail.com>
2023-08-02 17:12:38 -0500

    Added a Functions Module

--- ToDoListApp/main.py
+++ ToDoListApp/main.py
@@ -1,20 +1,6 @@
-
-
-def get_tasks(filepath='tasks.txt'):
-    """ Read a task and return a list tasks
-    from the tasks text file.
-    """
-    with open(filepath, 'r') as file_local:
-        tasks_local = file_local.readlines()
-    return tasks_local
-
-def write_tasks(tasks_arg, filepath="tasks.txt"):
-    """ Write a task and return a list of tasks
-    from the tasks text file.
-    """
-    with open(filepath, 'w') as file:
-        file.writelines(tasks_arg)
-
+# from functions import get_tasks, write_tasksimport functions
+# from modules import functions
+import functions
 
 while True:
     user_action = input("Type Add, Show, Edit, Complete or Exit: ")
@@ -23,15 +9,15 @@ while True:
     if user_action.startswith("Add"):
         task = user_action[4:]
 
-        tasks = get_tasks()
+        tasks = functions.get_tasks()
 
         tasks.append(task + '\n')
 
-        write_tasks(tasks)
+        functions.write_tasks(tasks)
 
     elif user_action.startswith('Show'):
 
-        tasks = get_tasks()
+        tasks = functions.get_tasks()
 
         for index, item in enumerate(tasks):
             item = item.strip('\n')
@@ -43,12 +29,12 @@ while True:
             number = int(user_action[5:])
             number = number - 1
 
-            tasks = get_tasks()
+            tasks = functions.get_tasks()
 
             new_task = input("Enter New Task: ")
             tasks[number] = new_task + '\n'
 
-            write_tasks(tasks)
+            functions.write_tasks(tasks)
 
         except ValueError:
             print("Your Command is Not Valid, Please try again.")
@@ -58,13 +44,13 @@ while True:
         try:
             number = int(user_action[9:])
 
-            tasks = get_tasks()
+            tasks = functions.get_tasks()
 
             index = number - 1
             tasks_to_remove = tasks[index].strip('\n')
             tasks.pop(number - 1)
 
-            write_tasks(tasks)
+            functions.write_tasks(tasks)
 
             message = f'task {tasks_to_remove} was removed from the list.'
             print(message)
